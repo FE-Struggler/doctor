2787f35ca8a0fd5e320da7c496f9241027e1dc62
yang <2365539910@qq.com>
2023-06-23 23:49:15 +0800

    fix: check Chrome

--- src/features/web-tools/checkChrome.ts
+++ src/features/web-tools/checkChrome.ts
@@ -2,15 +2,19 @@ import { IApi } from "src/types";
 import { exec } from 'child_process'
 
 async function isChromeInstalled(): Promise<boolean> {
-  const stdout: string = await execCommand('sudo -S /Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --version') as string
-  if (stdout.startsWith('Google Chrome')) {
-    return true;
+  try {
+    const stdout: string = await execCommand('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --version');
+    if (stdout.startsWith('Google Chrome')) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
   }
-  return false;
 }
 
-// 执行命令，并返回命令输出的结果
-function execCommand(command: string) {
+function execCommand(command: string): Promise<string> {
   return new Promise((resolve, reject) => {
     const child = exec(command, (error, stdout, stderr) => {
       if (error) {
@@ -19,12 +23,10 @@ function execCommand(command: string) {
         resolve(stdout.trim());
       }
     });
-    child.on('exit', (code) => {
-      if (code === 127) { // command not found
-        reject(new Error(`Command not found: ${command}`));
-      }
+
+    child.on('close', () => {
+      child.kill();
     });
-   
   });
 }
 
@@ -35,7 +37,7 @@ export default (api: IApi) => {
       return {
         label: 'isChromeInstalled',
         description: 'You should apply Chrome for web development',
-        type:'error'
+        type: 'error'
       }
     }
   })
