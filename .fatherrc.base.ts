106ed72aae85b0a13ff7cbf32971b0e65e83c9fe
snowingfox <1503401882@qq.com>
2023-06-26 13:06:32 +0800

    feat: remove alias

--- .fatherrc.base.ts
+++ .fatherrc.base.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "father";
-import { alias } from "./alias";
 
 export default defineConfig({
   cjs: {
     output: "./dist",
-    alias,
   },
   prebundle: {
     deps: {},
