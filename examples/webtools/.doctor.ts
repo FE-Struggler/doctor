2db618a3a477343ab3ea293729ad34e73ba4b5eb
lijiayang <rvjiangli_i@didiglobal.com>
2023-06-26 20:49:53 +0800

    test: add vscode plugins test case

--- examples/webtools/.doctor.ts
+++ examples/webtools/.doctor.ts
@@ -3,6 +3,9 @@ import { defineConfig, DoctorLevel } from "@doctors/core";
 export default defineConfig({
   tools: {
     nodeVersion: DoctorLevel.WARN,
-    vscodePlugins: ["volar", "Vue.volar", "github"],
+    // vscodePlugins: ["volar", "Vue.volar", "github"],
+    vscode: {
+      morePlugins: [{ name: "Vue.volar", desc: "自定义描述", version: "1.0" }],
+    },
   },
 });
